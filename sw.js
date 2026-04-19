2139254ff33827ddd12ee270a64b544dec194f80
Tomsen3 <tom.jansen@singende-krankenhaeuser.de>
2026-04-19 07:22:34 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+var CACHE = "singleiter-v1.4.1";
+var URLS = ["/"];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(URLS); }));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e) {
+  if (e.request.method !== "GET") return;
+  e.respondWith(fetch(e.request).then(function(r) {
+    var clone = r.clone();
+    caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
+    return r;
+  }).catch(function() {
+    return caches.match(e.request);
+  }));
+});
+
+self.addEventListener("message", function(e) {
+  if (e.data === "skipWaiting") self.skipWaiting();
+});
