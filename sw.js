f59624879a091ecdc2d67abadaf76144f0f1704e
Tomsen3 <tom.jansen@singende-krankenhaeuser.de>
2026-04-19 09:43:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "singleiter-v1.4.2";
+var CACHE = "singleiter-v1.4.3";
 var URLS = ["/singleiter/", "/singleiter/index.html", "/singleiter/sw.js", "/singleiter/manifest.json", "/singleiter/version.json"];
 
 self.addEventListener("install", function(e) {
